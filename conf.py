c4e186d30dbaa6e54d72fdfa0ba71cd828b5409e
Caleb Bell <Caleb.Andrew.Bell@gmail.com>
2016-01-02 20:22:31 -0400

    setup.py and conf mock updates

--- conf.py
+++ conf.py
@@ -15,6 +15,20 @@
 import sys
 import os
 
+
+import sys
+from mock import Mock as MagicMock
+
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+            return Mock()
+
+MOCK_MODULES = ['scipy', 'scipy.interpolate', 'scipy.constants', 'argparse', 'numpy', 'pandas']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
