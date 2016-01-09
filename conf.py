6ebcd6abd7f98517cd5f319bfda02cee8eeb8d48
Caleb Bell <Caleb.Andrew.Bell@gmail.com>
2016-01-09 18:01:55 -0400

    Readthedocs requirements.txt

--- conf.py
+++ conf.py
@@ -25,8 +25,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['scipy', 'scipy.interpolate', 'scipy.constants', 'argparse',
-'numpy', 'pandas']
+MOCK_MODULES = ['argparse', 'pandas']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
