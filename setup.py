e6a53e50e0553dd71b7b45848ad1b85f670e5cf8
Caleb Bell <Caleb.Andrew.Bell@gmail.com>
2016-03-06 12:16:09 -0400

    0.1.27 release

--- setup.py
+++ setup.py
@@ -53,8 +53,8 @@ setup(
   name = 'fluids',
   packages = ['fluids'],
   license='GPL3',
-  version = '0.1.26',
-  download_url = 'https://github.com/CalebBell/fluids/tarball/0.1.26',
+  version = '0.1.27',
+  download_url = 'https://github.com/CalebBell/fluids/tarball/0.1.27',
   description = 'Fluid dynamics component of Chemical Engineering Design Library (ChEDL)',
   long_description = long_description,
   extras_require = {
