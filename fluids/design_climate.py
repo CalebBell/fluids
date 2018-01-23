d97f208f365aeaa217dffc3edee537f779a69a04
Caleb Bell <Caleb.Andrew.Bell@gmail.com>
2018-01-22 22:56:04 -0700

    Add gsod_day_parser parser

--- fluids/design_climate.py
+++ fluids/design_climate.py
@@ -27,14 +27,13 @@ except: # pragma: no cover
     from io import BytesIO as StringIO
 import os
 import gzip
+import datetime
 
 import numpy as np
 from fluids.core import F2K
 from scipy.constants import mile, knot, inch
 from scipy.spatial import KDTree, cKDTree
 from scipy.stats import scoreatpercentile
-
-#F2K = lambda F : convert_temperature(F, 'f', 'k')
 
 
 try: # pragma: no cover
@@ -263,3 +262,127 @@ def get_station_year_text(station, year):
     
 
 
+gsod_fields = ['DATE', # 15-18 int year; 19-22 int month/day
+               'TEMP', # 25-30 Real Mean temperature for the day in degrees Fahrenheit to tenths. Missing = 9999.9
+               'TEMP_COUNT', # 32-33 Int. Number of observations used in calculating mean temperature
+               'DEWP', # 36-41 Real Mean dew point for the day in degrees Fahrenheit to tenths.  Missing = 9999.9
+               'DEWP_COUNT', # 43-44 Int. Number of observations used in calculating mean dew point
+               'SLP', # 47-52 Real Mean sea level pressure for the day in millibars to tenths.  Missing = 9999.9
+               'SLP_COUNT', # 54-55 Int. Number of observations used in calculating mean sea level pressure
+               'STP', # 58-63 Real Mean station pressure for the day in millibars to tenths. Missing = 9999.9
+               'STP_COUNT', # 65-66 Int. Number of observations used in calculating mean station pressure
+               'VISIB', # 69-73 Real Mean visibility for the day in miles to tenths. Missing = 999.9
+               'VISIB_COUNT', # 75-76 Int. Number of observations used in calculating mean visibility
+               'WDSP', # 79-83 Real Mean wind speed for the day in knots to tenths. Missing = 999.9
+               'WDSP_COUNT', # 85-86 Int. Number of observations used in calculating mean wind speed
+               'MXSPD', # 89-93 Real Maximum sustained wind speed reported for the day in knots to tenths. Missing = 999.9
+               'GUST', # 96-100 Real Maximum wind gust reported for the day in knots to tenths. Missing = 999.9
+               'MAX', # 103-108 Real Maximum temperature reported during the 
+                      # day in Fahrenheit to tenths--time of max temp report varies by country and
+                      # region, so this will sometimes not be the max for the calendar day.
+                      # Missing = 9999.9; FLAG of '*' is present on 109-109!
+               'MIN', # 111-116 Real Minimum temperature reported during the day in Fahrenheit to tenths--time of min
+                      # temp report varies by country and region, so this will sometimes not be 
+                      # the min for the calendar day. Missing = 9999.9 FLAG of '*' is present on 117-117!
+               'PRCP', # 119-123 Real Total precipitation (rain and/or melted snow) reported during the day in inches
+                       # and hundredths; will usually not end with the midnight observation--i.e.,
+                       # may include latter part of previous day. .00 indicates no measurable
+                       # precipitation (includes a trace).
+                       # Missing = 99.99
+
+               'SNDP', # 126-130 Real Snow depth in inches to tenths--last report for the day if reported more than
+                       # once.  Missing = 999.9 Note: Most stations do not report '0' on days with no snow on the
+                       # ground--therefore, '999.9' will often appear on these days.
+               'FRSHTT' # 133-138 Int. Indicators (1 = yes, 0 = no/not reported) for the occurrence during the day of:
+                        # Fog ('F' - 1st digit).
+                        # Rain or Drizzle ('R' - 2nd digit).
+                        # Snow or Ice Pellets ('S' - 3rd digit).
+                        # Hail ('H' - 4th digit).
+                        # Thunder ('T' - 5th digit).
+                        # Tornado or Funnel Cloud ('T' - 6th digit).              
+              ]
+
+# Values to be converted to floats always
+gsod_float_fields = ('TEMP', 'DEWP', 'SLP', 'STP', 'VISIB', 'WDSP', 'MXSPD', 
+                     'GUST', 'MAX', 'MIN', 'PRCP', 'SNDP')
+# Values to be converted to ints always
+gsod_int_fields = ('TEMP_COUNT', 'DEWP_COUNT', 'SLP_COUNT', 'STP_COUNT', 
+                   'VISIB_COUNT', 'WDSP_COUNT')
+
+# Values which signify flags
+gsod_flag_chars = '*ABCDEFGHI'
+# Values which should be converted to None, as normally there is no value
+gsod_bad_values = set(['99.99', '999.9', '9999.9'])
+
+gsod_indicator_names = ('fog', 'rain', 'snow_ice', 'hail', 'thunder', 
+                        'tornado')
+five_ninths = 5.0/9.0
+
+
+def gsod_datetime_parser(yymmdd):
+    if not yymmdd:
+        return yymmdd
+    return datetime.datetime.strptime(yymmdd, '%Y%m%d')
+
+
+def gsod_day_parser(line, SI=True, datetime=True):
+    # Ignore STN--- and WBAN, 8-12 characters
+    fields = line.strip().split()[2:]
+    # For the case the field is blank, set it to None; strip it either way 
+    for i in range(len(fields)):
+        field = fields[i].strip()
+        if not field:
+            field = None
+        fields[i] = field 
+
+    obj = dict(zip(gsod_fields, fields))
+    if datetime:
+        # Convert the date to a datetime object
+        obj['DATE'] = gsod_datetime_parser(obj['DATE'])
+    
+    # Parse float values as floats
+    for field in gsod_float_fields:
+        value = obj[field].rstrip(gsod_flag_chars)
+        if value in gsod_bad_values:
+            value = None
+        else:
+            value = float(value)
+        obj[field] = value
+        
+    if SI:
+        # All temperatures are in deg F
+        for field in ('TEMP', 'DEWP', 'MAX', 'MIN'):
+            value = obj[field]
+            if value is not None:
+                obj[field] = (value + 459.67)*five_ninths
+
+            
+        # Convert visibility, wind speed, pressures
+        # to si units of meters, Pascal, and meters/second.
+        if obj['VISIB'] is not None:
+            obj['VISIB'] = obj['VISIB']*mile
+        if obj['PRCP'] is not None:
+            obj['PRCP'] = obj['PRCP']*inch
+        if obj['SNDP'] is not None:
+            obj['SNDP'] = obj['SNDP']*inch
+        if obj['WDSP'] is not None:
+            obj['WDSP'] = obj['WDSP']*knot 
+        if obj['MXSPD'] is not None:
+            obj['MXSPD'] = obj['MXSPD']*knot
+        if obj['GUST'] is not None:
+            obj['GUST'] = obj['GUST']*knot
+        if obj['SLP'] is not None:
+            obj['SLP'] = obj['SLP']*100.0
+        if obj['STP'] is not None:
+            obj['STP'] = obj['STP']*100.0
+
+    # Parse int values as ints
+    for field in gsod_int_fields:
+        value = obj[field] 
+        if value is not None:
+            obj[field] = int(value)
+
+    indicator_values = [flag == '1' for flag in obj['FRSHTT']]
+    obj.update(zip(gsod_indicator_names, indicator_values))
+    
+    return obj
