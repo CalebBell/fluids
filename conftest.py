6a20e27dd598d1bb57e492258da2a45afad29257
Caleb Bell <Caleb.Andrew.Bell@gmail.com>
2020-07-28 21:56:41 -0600

    Get doctests only running on python 3.7

--- conftest.py
+++ conftest.py
@@ -1,4 +1,5 @@
 import sys
+import platform
 
 def pytest_ignore_collect(path):
     path = str(path)
@@ -7,3 +8,6 @@ def pytest_ignore_collect(path):
     if sys.version[0] == '2':
         if 'numba' in path or 'typing_utils' in path:
             return True
+        if 'rst' in path:
+            if platform.python_version_tuple()[0:2] != ('3', '7'):
+                return True
