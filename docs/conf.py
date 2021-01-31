c72fe7d6397af824f89a5c0b4e99eb41cb00a0f3
Caleb Bell <Caleb.Andrew.Bell@gmail.com>
2021-01-31 12:45:47 -0700

    Update copyright year on readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -78,7 +78,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Fluids'
-copyright = u'2016 - 2020, Caleb Bell <Caleb.Andrew.Bell@gmail.com>'
+import datetime
+copyright = u'2016 - %s, Caleb Bell <Caleb.Andrew.Bell@gmail.com>' %datetime.datetime.now().year
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
