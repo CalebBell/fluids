9611e95ea7b9a096a1f498438bdbb370a272e97c
Caleb Bell <Caleb.Andrew.Bell@gmail.com>
2023-01-02 21:00:39 -0700

    Another function

--- fluids/numerics/__init__.py
+++ fluids/numerics/__init__.py
@@ -34,7 +34,7 @@ from fluids.numerics.arrays import (solve as py_solve, inv, dot, norm2, inner_pr
 from fluids.numerics.special import (py_hypot, py_cacos, py_catan, py_catanh, 
                                      trunc_exp, trunc_log)
 
-from fluids.numerics.polynomial_roots import roots_quadratic
+from fluids.numerics.polynomial_roots import (roots_quadratic, roots_quartic)
 
 
 __all__ = ['isclose', 'horner', 'horner_and_der', 'horner_and_der2',
@@ -520,51 +520,6 @@ def roots_cubic(a, b, c, d):
     return (x1, x2, x3)
 
 
-def roots_quartic(a, b, c, d, e):
-    # There is no divide by zero check. A should be 1 for best numerical results
-    # Multiple order of magnitude differences still can cause problems
-    # Like  [1, 0.0016525874561771799, 106.8665062954208, 0.0032802613917246727, 0.16036091315844248]
-    x0 = 1.0/a
-    x1 = b*x0
-    x2 = -x1*0.25
-    x3 = c*x0
-    x4 = b*b*x0*x0
-    x5 = -two_thirds*x3 + 0.25*x4
-    x6 = x3 - 0.375*x4
-    x6_2 = x6*x6
-    x7 = x6_2*x6
-    x8 = d*x0
-    x9 = x1*(-0.5*x3 + 0.125*x4)
-    x10 = (x8 + x9)*(x8 + x9)
-    x11 = e*x0
-    x12 = x1*(x1*(-0.0625*x3 + 0.01171875*x4) + 0.25*x8) # 0.01171875 = 3/256
-    x13 = x6*(x11 - x12)
-    x14 = -.125*x10 + x13*third - x7/108.0
-    x15 = 2.0*(x14 + 0.0j)**(third)
-    x16 = csqrt(-x15 + x5)
-    x17 = 0.5*x16
-    x18 = -x17 + x2
-    x19 = -four_thirds*x3
-    x20 = 0.5*x4
-    x21 = x15 + x19 + x20
-    x22 = 2.0*x8 + 2.0*x9
-    x23 = x22/x16
-    x24 = csqrt(x21 + x23)*0.5
-    x25 = -x11 + x12 - twelfth*x6_2
-    x27 = (0.0625*x10 - x13*sixth + x7/216.0 + csqrt(0.25*x14*x14 + one_27*x25*x25*x25))**(third)
-    x28 = 2.0*x27
-    x29 = two_thirds*x25/(x27)
-    x30 = csqrt(x28 - x29 + x5)
-    x31 = 0.5*x30
-    x32 = x2 - x31
-    x33 = x19 + x20 - x28 + x29
-    x34 = x22/x30
-    x35 = csqrt(x33 + x34)*0.5
-    x36 = x17 + x2
-    x37 = csqrt(x21 - x23)*0.5
-    x38 = x2 + x31
-    x39 = csqrt(x33 - x34)*0.5
-    return ((x32 - x35), (x32 + x35), (x38 - x39), (x38 + x39))
 
 def mean(data):
     # Much faster than the statistics.mean module
--- fluids/numerics/polynomial_roots.py
+++ fluids/numerics/polynomial_roots.py
@@ -26,6 +26,19 @@ from math import (sin, exp, pi, fabs, copysign, log, isinf, isnan, acos, cos, si
                   atan2, asinh, sqrt, gamma)
 from cmath import sqrt as csqrt, log as clog
 
+__all__ = ['roots_quadratic', 'roots_quartic']
+third = 1.0/3.0
+sixth = 1.0/6.0
+ninth = 1.0/9.0
+twelfth = 1.0/12.0
+two_thirds = 2.0/3.0
+four_thirds = 4.0/3.0
+
+
+root_three = 1.7320508075688772 # sqrt(3.0)
+one_27 = 1.0/27.0
+complex_factor = 0.8660254037844386j # (sqrt(3)*0.5j)
+
 def roots_quadratic(a, b, c):
     if a == 0.0:
         root = -c/b
@@ -41,3 +54,49 @@ def roots_quadratic(a, b, c):
         x1 = (D - b)*a_inv_2
         x2 = -(b + D)*a_inv_2
     return (x1, x2)
+
+def roots_quartic(a, b, c, d, e):
+    # There is no divide by zero check. A should be 1 for best numerical results
+    # Multiple order of magnitude differences still can cause problems
+    # Like  [1, 0.0016525874561771799, 106.8665062954208, 0.0032802613917246727, 0.16036091315844248]
+    x0 = 1.0/a
+    x1 = b*x0
+    x2 = -x1*0.25
+    x3 = c*x0
+    x4 = b*b*x0*x0
+    x5 = -two_thirds*x3 + 0.25*x4
+    x6 = x3 - 0.375*x4
+    x6_2 = x6*x6
+    x7 = x6_2*x6
+    x8 = d*x0
+    x9 = x1*(-0.5*x3 + 0.125*x4)
+    x10 = (x8 + x9)*(x8 + x9)
+    x11 = e*x0
+    x12 = x1*(x1*(-0.0625*x3 + 0.01171875*x4) + 0.25*x8) # 0.01171875 = 3/256
+    x13 = x6*(x11 - x12)
+    x14 = -.125*x10 + x13*third - x7/108.0
+    x15 = 2.0*(x14 + 0.0j)**(third)
+    x16 = csqrt(-x15 + x5)
+    x17 = 0.5*x16
+    x18 = -x17 + x2
+    x19 = -four_thirds*x3
+    x20 = 0.5*x4
+    x21 = x15 + x19 + x20
+    x22 = 2.0*x8 + 2.0*x9
+    x23 = x22/x16
+    x24 = csqrt(x21 + x23)*0.5
+    x25 = -x11 + x12 - twelfth*x6_2
+    x27 = (0.0625*x10 - x13*sixth + x7/216.0 + csqrt(0.25*x14*x14 + one_27*x25*x25*x25))**(third)
+    x28 = 2.0*x27
+    x29 = two_thirds*x25/(x27)
+    x30 = csqrt(x28 - x29 + x5)
+    x31 = 0.5*x30
+    x32 = x2 - x31
+    x33 = x19 + x20 - x28 + x29
+    x34 = x22/x30
+    x35 = csqrt(x33 + x34)*0.5
+    x36 = x17 + x2
+    x37 = csqrt(x21 - x23)*0.5
+    x38 = x2 + x31
+    x39 = csqrt(x33 - x34)*0.5
+    return ((x32 - x35), (x32 + x35), (x38 - x39), (x38 + x39))
