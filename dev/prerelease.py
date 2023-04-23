a3ed24b7c6b3c7bc616cc7f0ee442815c6eb63c2
Caleb Bell <Caleb.Andrew.Bell@gmail.com>
2023-04-23 13:23:27 -0600

    Disable monkeytype for now

--- dev/prerelease.py
+++ dev/prerelease.py
@@ -2,8 +2,8 @@ import sys
 import os
 import importlib.util
 
-if sys.version_info.major != 3 and sys.version_info.minor != 7:
-	raise ValueError("""This prerelease script will only run on Python 3.7.
+if sys.version_info.major != 3 and sys.version_info.minor != 11:
+	raise ValueError("""This prerelease script will only run on Python 3.11.
 Some parts of a library change the last few decimals numbers between releases,
 and other parts only have obsolete dependencies i.e. pint on Python 2.
 For that reason, while the pytest test suite runs everywhere,
@@ -17,7 +17,10 @@ def set_file_modification_time(filename, mtime):
     
 now = datetime.now()
 
-paths = ['..']
+main_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+
+
+paths = [main_dir]
 
 for p in paths:
     for (dirpath, dirnames, filenames) in os.walk(p):
@@ -29,14 +32,13 @@ for p in paths:
 
 
 import fluids
-main_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 test_dir = os.path.join(main_dir, 'tests')
 os.chdir(test_dir)
 
-mod_spec = importlib.util.spec_from_file_location("make_test_stubs", os.path.join(test_dir, "make_test_stubs.py"))
-make_test_stubs = importlib.util.module_from_spec(mod_spec)
-mod_spec.loader.exec_module(make_test_stubs)
+# mod_spec = importlib.util.spec_from_file_location("make_test_stubs", os.path.join(test_dir, "make_test_stubs.py"))
+# make_test_stubs = importlib.util.module_from_spec(mod_spec)
+# mod_spec.loader.exec_module(make_test_stubs)
 
 import pytest
 os.chdir(main_dir)
-pytest.main(["--doctest-glob='*.rst'", "--doctest-modules", "--nbval", "-n", "3", "--dist", "loadscope", "-v"])
+pytest.main(["--doctest-glob='*.rst'", "--doctest-modules", "--nbval", "-n", "8", "--dist", "loadscope", "-v"])
